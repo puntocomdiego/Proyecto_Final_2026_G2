cb2e3cae37436ecedef1eb48c1a63f5a2a174172
diego aldorasi <dialdo@hotmail.com>
2026-09-16 15:32:51 -0300

    pequeña prueba

--- Frm_Notas_profe.Designer.vb
+++ Frm_Notas_profe.Designer.vb
@@ -22,10 +22,29 @@ Partial Class Frm_Notas_profe
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(93, 56)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(146, 52)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Frm_Notas_profe
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 450)
+        Controls.Add(Button1)
+        Name = "Frm_Notas_profe"
+        Text = "Form1"
+        ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Button1 As Button
 
 End Class
