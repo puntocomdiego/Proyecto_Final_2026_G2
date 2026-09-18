b89cbac12dfad14ffc3d8f46a0367d073cefb26a
5upr3m4 <dossantosteo0@gmail.com>
2026-09-17 22:01:14 -0300

    frm creado Alumno 1 22:01

---
+++ Alumno1/frmProductos.Designer.vb
@@ -0,0 +1,49 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class frmProductos
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Button1 = New Button()
+        SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(332, 64)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(151, 67)
+        Button1.TabIndex = 0
+        Button1.Text = "boton de muestra"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' frmProductos
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(800, 372)
+        Controls.Add(Button1)
+        Name = "frmProductos"
+        Text = "frmProductos"
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Button1 As Button
+End Class
