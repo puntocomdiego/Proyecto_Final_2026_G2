1a6547f8ba473a79a78bb4dca71e30d004615960
diego aldorasi <dialdo@hotmail.com>
2026-09-18 00:18:16 -0300

    listo. carpeta clases y modulo

---
+++ Clases/readme.txt
@@ -0,0 +1,4 @@
+﻿las clases deben ser nombradas con mayusculas 
+y en singular, por ejemplo: Cls_Persona, Cls_Producto, 
+Cls_Cliente, etc.	
+
---
+++ Conexion.vb
@@ -0,0 +1,4 @@
+﻿Module Conexion
+    Public ReadOnly Ruta As String = IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "nombre_de_la_base.accdb")
+    Public ReadOnly CadenaConexion As String = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={Ruta};"
+End Module
